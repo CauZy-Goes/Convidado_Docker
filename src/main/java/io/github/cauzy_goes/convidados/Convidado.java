e618d22a21dd21d5776f0be4e2da1d10681d83a6
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-24 09:48:16 -0300

    feat: create entity convidado

--- src/main/java/io/github/cauzy_goes/convidados/Convidado.java
+++ src/main/java/io/github/cauzy_goes/convidados/Convidado.java
@@ -1,12 +1,24 @@
 package io.github.cauzy_goes.convidados;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Convidado {
-    private String nome;
+    @Id
+    @Column
     private String cpf;
+    @Column
+    private String nome;
 
     public Convidado(String nome , String cpf) {
         this.cpf = cpf;
         this.nome = nome;
+    }
+
+    public Convidado() {
+
     }
 
     public String getNome() {
---
+++ src/main/java/io/github/cauzy_goes/convidados/ConvidadosRepository.java
@@ -0,0 +1,6 @@
+package io.github.cauzy_goes.convidados;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConvidadosRepository extends JpaRepository<Convidado, String> {
+}
